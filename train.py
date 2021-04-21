48b6e004b703d7869af953828844e10060d98dcc
AllenAdom <enthusiast521@outlook.com>
2021-04-21 17:19:58 +0800

    Second push test...

--- train.py
+++ train.py
@@ -95,7 +95,7 @@ def invoke_model(actor_agent, obs, exp):
 
     return node, use_exec
 
-
+# Hello, this is a test !
 # --exec_cap 8 --num_init_dags 1 --num_stream_dags 50 --reset_prob 5e-7 --reset_prob_min 5e-8 --reset_prob_decay 4e-10
 # --diff_reward_enabled 1 --num_agents 2 --ba_size 8
 # --model_save_interval 100 --model_folder models/stream_200_job_diff_reward_reset_5e-7_5e-8/
